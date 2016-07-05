823250ac3db2858da3226caddf9c51d5e1799d49
Matteo Menapace <hello@baddeo.com>
2016-07-05 17:02:51 +0200

    More data points

--- index.js
+++ index.js
@@ -11,20 +11,98 @@ var csvOptions =
   fields: 
   [
     {
+      name : 'id',
+      label : 'id'
+    },
+    {
+      name : 'name',
+      label : 'name'
+    },
+    {
+      name : 'lat',
+      label : 'lat'
+    },
+    {
+      name : 'lng',
+      label : 'lng'
+    },
+    {
+      name : 'price_native',
+      label : 'price €'
+    },
+    {
+      name : 'bedrooms',
+      label : 'bedrooms'
+    },
+    {
+      name : 'beds',
+      label : 'beds'
+    },
+    {
+      name : 'person_capacity',
+      label : 'person capacity'
+    },
+    {
+      name : 'room_type',
+      label : 'room type'
+    },
+    {
+      name : 'property_type',
+      label : 'property type'
+    },
+    {
+      name : 'reviews_count',
+      label : 'reviews'
+    },
+    {
+      name : 'star_rating',
+      label : 'star rating'
+    }, 
+    {
+      name : 'user_id',
+      label : 'user_id'
+    },
+    {
+      name : 'user.user.reviewee_count',
+      label : 'user reviewed count'
+    },
+    {
+      name : 'picture_count',
+      label : 'picture count'
+    },
+    {
+      name : 'neighborhood',
+      label : 'neighborhood'
+    },
+    {
       name : 'city',
       label : 'city'
-    }/*,
-    {
-      name : 'contact.name',
-      label : 'Name'
     },
     {
-      name : 'contact.email',
-      label : 'Email'
+      name : 'house_rules',
+      label : 'house rules'
     },
     {
-      name : 'registration.year',
-      label : 'Year'
+      name : 'summary',
+      label : 'summary'
+    },
+    {
+      name : 'description',
+      label : 'description'
+    },
+    {
+      name : 'space',
+      label : 'space'
+    },
+    {
+      name : 'transit',
+      label : 'transit'
+    }
+
+    /*
+    {
+      name : '',
+      label : ''
     },
     {
       name : 'registration.level',
